ae13996910e5e115e846200f596e18af34b3d588
taufik-hdyt25 <taufikh2519@gmail.com>
2025-11-13 23:24:07 +0700

    fix prod

--- src/containers/WeddingInvitation/WeddingInvitation.tsx
+++ src/containers/WeddingInvitation/WeddingInvitation.tsx
@@ -4,9 +4,19 @@ import { IPagesParams } from "@/interfaces/IBaseParams";
 import { Box, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import { MainPage, OpenFirst } from "./Partials";
+import { configEnv } from "@/lib/configEnv";
 
 const WeddingInvitation: React.FC<IPagesParams> = ({ params }) => {
   const { isOpen, onOpen } = useDisclosure();
+  const isProd = configEnv.isProd || false;
+
+  if (isProd) {
+    return (
+      <Box w="100%" h="100vh">
+        {!isOpen ? <OpenFirst onOpenInviataion={onOpen} /> : <MainPage />}
+      </Box>
+    );
+  }
 
   return (
     <Box w="100%" h="100vh">
--- src/lib/configEnv.ts
+++ src/lib/configEnv.ts
@@ -3,4 +3,5 @@ export const configEnv = {
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   secret: process.env.NEXTAUTH_SECRET,
   baseUrl: process.env.NEXT_PUBLIC_BASE_API_URL,
+  isProd: process.env.NEXT_PUBLIC_PROD,
 };
