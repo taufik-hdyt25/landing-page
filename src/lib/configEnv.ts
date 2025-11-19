45990aec10d7bbb6fff39d849cb66ed9381a8e36
taufik-hdyt25 <taufikh2519@gmail.com>
2025-11-19 17:27:57 +0700

    fix

--- src/lib/configEnv.ts
+++ src/lib/configEnv.ts
@@ -1,7 +1,4 @@
 export const configEnv = {
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  secret: process.env.NEXTAUTH_SECRET,
   baseUrl: process.env.NEXT_PUBLIC_BASE_API_URL,
   isProd: process.env.NEXT_PUBLIC_PROD,
   weddingDate: process.env.NEXT_PUBLIC_DATE,
